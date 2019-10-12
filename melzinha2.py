1de34799c0cc5de3a6b1ac39fd7d1f022fa3c1d4
leticiatateishi <leticiatateishi@gmail.com>
2019-10-12 12:07:34 -0300

    Começando versão 2 do bot

---
+++ melzinha2.py
@@ -0,0 +1,48 @@
+# Letícia Mayumi A. Tateishi
+
+# encoding: utf-8
+# encoding: iso-8859-1
+# encoding: win-1252
+
+import telegram
+import logging
+import json
+from telegram.ext import Updater
+from telegram.ext import CommandHandler, MessageHandler, Filters
+
+
+"""Caminho do arquivo de configuração do tipo JSON."""
+caminho_configuracao = 'config.json'
+
+
+# Na thread inicial, configuramos e aguardamos as respostas do bot
+if __name__ == "__main__":
+	# Abrimos o arquivo de configuração como leitura
+	with open(caminho_configuracao, mode='rt', encoding='utf-8') as arquivo:
+		# Carregamos o JSON a partir do arquivo
+		configuracao = json.load(arquivo)
+
+	# Com o token, registramos o bot
+	bot = telegram.Bot(token='845551679:AAHJtCYhmzAfp4T1PA4Kqcc8iVfpADyCnYA')
+
+	def salvar_configuracao():
+		"""Salva a configuração do bot no arquivo."""
+		with open(caminho_configuracao, mode='wt', encoding='utf-8') as arquivo:
+			json.dump(configuracao, arquivo, indent='\t')
+
+
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+updater = Updater(token='845551679:AAHJtCYhmzAfp4T1PA4Kqcc8iVfpADyCnYA', use_context=True)
+dispatcher = updater.dispatcher
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Sou a Mel")
+
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+updater.start_polling()
